9487fee13968fe9b6f31c9f9338c632f7604b40f
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2018-07-30 16:35:10 +0700

    Implement app module using brew

--- module/app.sh
+++ module/app.sh
@@ -1,11 +1,22 @@
+BASE_DIR=$(dirname $0)/..
+BREWFILE_DIR=$BASE_DIR/brew
+BREW_FILE=$BREWFILE_DIR/Brewfile
+
 app_clean() {
-  echo "CLEAN"
+  echo "Uninstalling unused apps..."
+  brew bundle cleanup --force --file=$BREW_FILE
+  echo "Cleaning up packages..."
+  brew cleanup
 }
 
 app_install() {
-  echo "INSTALL"
+  echo "Installing apps..."
+  brew bundle --no-upgrade --file=$BREW_FILE
 }
 
-app_upgrade() {
-  echo "UPGRADE"
+app_update() {
+  echo "Updating apps..."
+  brew bundle --file=$BREW_FILE
+  echo "Updating packages..."
+  brew upgrade
 }
