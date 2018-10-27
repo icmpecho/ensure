95d8dd61d6333c2bc96a64ed20d262a78ff28419
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2018-10-27 17:11:28 +0700

    Add dot:link command

---
+++ dotfiles/misc/aliases.zsh
@@ -0,0 +1,15 @@
+#AWSume alias to source the AWSume script
+alias awsume=". awsume"
+# grc overides for ls
+#   Made possible through contributions from generous benefactors like
+#   `brew install coreutils`
+if $(gls &>/dev/null)
+then
+  alias ls="gls -F --color"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color"
+  alias la='gls -A --color'
+fi
+
+alias ack=ag
+alias vim=nvim
---
+++ dotfiles/nvm/load.zsh
@@ -0,0 +1,3 @@
+export NVM_DIR="$HOME/.nvm"
+mkdir -p $NVM_DIR
+. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
