8c91bba09e8b5d473e810bc119837f2b5abf7199
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2019-01-06 14:31:28 +0700

    Install coreutils during bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 set -e
 echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing coreutils..."
+brew install coreutils
 echo "Installing ensure command..."
 ln -sf $HOME/.ensure/bin/ensure /usr/local/bin/
 echo "Installed $HOME/.ensure/bin/ensure -> /usr/local/bin/ensure"
