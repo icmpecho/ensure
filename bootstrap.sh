ae302d33ee9974d09e428374ddd9e978aa5a1e58
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2023-01-17 17:30:28 +0700

    Update brew and nvm initialization

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Installing Homebrew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "Installing coreutils..."
 brew install coreutils
 echo "Installing ensure command..."
